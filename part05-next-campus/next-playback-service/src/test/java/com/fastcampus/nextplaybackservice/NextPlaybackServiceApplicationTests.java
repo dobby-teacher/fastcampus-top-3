221b8f54c8f1f8b2b21961a1be2a0ffce6781ea9
dobby-teacher <admin@dobby.work>
2024-05-05 16:21:08 +0900

    feat: 5-5-2 컨텐츠 재생 정보 서비스 컴포넌트 개발

---
+++ part05-next-campus/next-playback-service/src/test/java/com/fastcampus/nextplaybackservice/NextPlaybackServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.nextplaybackservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextPlaybackServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

a26fa456d2ad2e1f070cf93490e28324e681d6bd
dobby-teacher <admin@dobby.work>
2024-05-11 21:09:00 +0900

    test: http test 추가

--- part05-next-campus/next-course-service/src/main/resources/sql/schema-h2.sql
+++ part05-next-campus/next-course-service/src/main/resources/sql/schema-h2.sql
@@ -1,6 +1,6 @@
 CREATE TABLE COURSES
 (
-    course_id     INTEGER PRIMARY KEY,
+    course_id     INTEGER AUTO_INCREMENT PRIMARY KEY,
     title         VARCHAR(255) NOT NULL,
     description   TEXT,
     instructor_id INT NOT NULL,
@@ -9,7 +9,7 @@ CREATE TABLE COURSES
 
 CREATE TABLE COURSE_SESSIONS
 (
-    session_id INTEGER PRIMARY KEY,
+    session_id INTEGER AUTO_INCREMENT PRIMARY KEY,
     course_id  INT NOT NULL,
     title      VARCHAR(255) NOT NULL,
     FOREIGN KEY (course_id) REFERENCES COURSES(course_id)
@@ -17,7 +17,7 @@ CREATE TABLE COURSE_SESSIONS
 
 CREATE TABLE COURSE_RATINGS
 (
-    rating_id  INTEGER PRIMARY KEY,
+    rating_id  INTEGER AUTO_INCREMENT PRIMARY KEY,
     course_id  INT NOT NULL,
     user_id    INT NOT NULL,
     rating     INTEGER NOT NULL,

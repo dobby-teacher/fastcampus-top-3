62102c7d3e4bf8dff79b649c51da5fe181827b2e
dobby-teacher <admin@dobby.work>
2024-06-07 16:18:33 +0900

    feat: Brave, Zipkin을 사용한 분산 로깅 구현

--- part05-next-campus/next-graphql/src/main/java/com/fastcampus/nextgraphql/config/AppConfig.java
+++ part05-next-campus/next-graphql/src/main/java/com/fastcampus/nextgraphql/config/AppConfig.java
@@ -1,5 +1,6 @@
 package com.fastcampus.nextgraphql.config;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,7 +10,7 @@ import org.springframework.web.client.RestTemplate;
 public class AppConfig {
     @Bean
     @LoadBalanced
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
     }
 }

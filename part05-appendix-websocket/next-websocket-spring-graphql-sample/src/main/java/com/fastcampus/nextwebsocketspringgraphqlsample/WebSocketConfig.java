fdd0038748394a383b14cb47cade64c0bd1df699
dobby-teacher <admin@dobby.work>
2024-07-21 12:38:17 +0900

    fix: websocket config 추가

---
+++ part05-appendix-websocket/next-websocket-spring-graphql-sample/src/main/java/com/fastcampus/nextwebsocketspringgraphqlsample/WebSocketConfig.java
@@ -0,0 +1,16 @@
+package com.fastcampus.nextwebsocketspringgraphqlsample;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/graphql").setAllowedOrigins("*").withSockJS();
+    }
+}
